698d148dd535f6f492bd28a3bdd08b086a301c01
BrenDev0 <lahey1991@gmail.com>
2025-07-08 18:00:13 -0600

    added takss to docs

--- dist/src/core/swagger/swagger.js
+++ dist/src/core/swagger/swagger.js
@@ -203,6 +203,7 @@ const endpointsFiles = [
     '../../modules/opportunities/pipelines/pipelines.routes.ts',
     '../../modules/opportunities/stages/stages.routes.ts',
     '../../modules/permissions/permissions.routes.ts',
+    '../../modules/tasks/tasks.routes.ts',
     '../../modules/tokens/tokens.routes.ts',
     '../../modules/users/users.routes.ts'
 ];
